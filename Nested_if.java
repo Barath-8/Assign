e04b0561541af515785e94e3a153d3b0ba5e6e7a
Barath <112636578+Barath-8@users.noreply.github.com>
2022-09-07 15:45:38 +0530

    Update Nested_if.java

--- Nested_if.java
+++ Nested_if.java
@@ -4,17 +4,12 @@ public class Nested_if {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int age =20;
-//		
-//		if(age >=60 )
-//			System.out.println("You are Old");
-//		else if(age >= 40)
-//			System.out.println("You are Middle aged");
-//		else if(age >18)
-//			System.out.println("You are an Adult");
-//		else
-//			System.out.println("You are a Minor");
-//		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your Age : ");
+		int age = sc.nextInt();
+		sc.close();
+		
 		if(age >18) {
 			if(age >=40 ) {
 				if(age >= 60)
