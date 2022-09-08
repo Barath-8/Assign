b7db04dd46fdcd6e0ca410fda5cdc5179655f7b5
Barath <112636578+Barath-8@users.noreply.github.com>
2022-09-08 21:58:13 +0530

    8.09.2022 // Arrays problem

---
+++ Matrix_Mul.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Matrix_Mul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr1[][]= {{1,2},{3,4}},
+			arr2[][]= {{5,6,7},{8,9,10}};
+		
+		int res[][] =new int[arr1.length][arr2[0].length];
+		
+		int len = res.length,
+			innerLen=res[0].length;
+		
+		for(int i=0 ; i<len  ; i++) 
+			for(int j=0 ; j<innerLen ; j++) 
+				for(int k =0 ; k<len ; k++)
+					res[i][j]+=(arr1[i][k]*arr2[k][j]);
+			
+		
+		
+
+		for(int i=0 ; i<len  ; i++) {
+			for(int j=0 ; j<innerLen ; j++) 
+				System.out.print(res[i][j]+" ");
+			
+			System.out.println();
+		}
+		
+		System.out.println(Arrays.deepToString(res));
+		
+	}
+
+}
