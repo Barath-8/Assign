afa841fe3775ee497f3cc05d9a4ada79423c31d7
Barath <112636578+Barath-8@users.noreply.github.com>
2022-10-28 14:26:26 +0530

    Add files via upload

---
+++ Oct28_Test/Ques4_V2.java
@@ -0,0 +1,45 @@
+import java.util.Map;
+import java.util.HashMap;
+//import java.util.Scanner;
+public class Ques4_V2 {
+	
+	//Equal arrays or not
+
+	public static void main(String[] args) {
+		
+		int nums1[]= {1,2,2},
+			nums2[]= {1,2,2,3};
+		
+		System.out.println("The two arrays are "+getRes(nums1,nums2));
+
+	}
+
+	private static String getRes(int[] nums1, int[] nums2) {
+		
+		if(nums1.length!=nums2.length) return "Not equal";
+		
+		Map<Integer,Integer> map =new HashMap<>();
+		
+		for(int val : nums1)
+			map.put(val, map.getOrDefault(val, 0)+1);
+		
+
+		for(int val : nums2)
+			if(!map.containsKey(val))
+				return "Not equal";
+			else {
+				int count = map.get(val)-1;
+				
+				if(count==0)
+					map.remove(val);
+				else
+					map.put(val, count);
+			}
+			
+		if(map.isEmpty())
+			return "Equal";
+		
+		return "Not equal";
+	}
+
+}
